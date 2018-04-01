0f016c4230e57de87cc7f1390887fcb21af37fb6
tomg <tomgeurtsen@gmail.com>
2018-04-01 20:12:37 +0200

    Implemented RestClient for NS calls

---
+++ app/src/main/java/com/geuso/disrupty/model/ns/NsRestClient.kt
@@ -0,0 +1,34 @@
+package com.geuso.disrupty.model.ns
+
+import android.preference.PreferenceManager
+import com.geuso.disrupty.App
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+
+object NsRestClient {
+
+    private val TAG = NsRestClient::class.qualifiedName
+    private const val BASE_URL = "http://webservices.ns.nl"
+
+    private val client = AsyncHttpClient()
+
+    init {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(App.context)
+
+        val username : String? = preferences.getString("api_ns_username", null)
+        val password : String? = preferences.getString("api_ns_password", null)
+
+        client.setBasicAuth(username, password)
+    }
+
+
+    fun get(url: String, params: RequestParams?, responseHandler: AsyncHttpResponseHandler) {
+        client.get(absoluteUrl(url), params, responseHandler)
+    }
+
+    private fun absoluteUrl(relativeUrl: String): String{
+        return BASE_URL + relativeUrl
+    }
+
+}

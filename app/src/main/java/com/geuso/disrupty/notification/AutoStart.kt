c4f904df4b10169e2e839713954937f15ce78f24
tomg <tomgeurtsen@gmail.com>
2018-09-22 18:13:51 +0200

    Migrated from AlarmManager to JobScheduler

--- app/src/main/java/com/geuso/disrupty/notification/AutoStart.kt
+++ app/src/main/java/com/geuso/disrupty/notification/AutoStart.kt
@@ -6,13 +6,8 @@ import android.content.Intent
 
 class AutoStart : BroadcastReceiver() {
 
-    private val disruptionCheckAlarm = DisruptionCheckAlarm()
-
-    override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent != null && intent.action == Intent.ACTION_BOOT_COMPLETED
-            && context != null) {
-            disruptionCheckAlarm.setAlarm(context)
-        }
+    override fun onReceive(context: Context, intent: Intent?) {
+        DisruptionCheckJobScheduler.scheduleDisruptionCheckJobIfRequired()
     }
 
 }
---
+++ app/src/main/java/com/geuso/disrupty/notification/DisruptionCheckJobService.kt
@@ -0,0 +1,32 @@
+package com.geuso.disrupty.notification
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import com.geuso.disrupty.disruption.DisruptionService
+
+
+class DisruptionCheckJobService : JobService() {
+
+    companion object {
+        private val TAG = DisruptionCheckJobService::class.qualifiedName
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        Log.d(TAG, "DisruptionCheckJob started")
+
+        Thread(Runnable {
+            DisruptionService.notifyDisruptedSubscriptions()
+            Log.d(TAG, "DisruptionCheckJob finished")
+            jobFinished(params, false)
+        }).start()
+
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        Log.d(TAG, "DisruptionCheckJob cancelled before completion")
+        return true
+    }
+
+}
